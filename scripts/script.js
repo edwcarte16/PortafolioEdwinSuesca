0015dcfe2e061fdaff147cc42843cd84b2e49e8e
Edwin Suesca <edwcarte16@gmail.com>
2021-04-30 23:56:33 -0500

    Se creó manú hamburguesa

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,28 @@
 var video = document.getElementById("videoDesigner1");
 var element = document.getElementById('videoDesigner1');
+var burgerMenu = document.getElementByClass('burgerMenuBox');
 
 
-function playFunction() {
+/*function playFunction() {
   if (video.paused) {
     video.play();
 
   } else {
     video.pause();
   }
+}*/
+
+function playFunction() {  
+    video.play();
+}
+
+function pauseFunction() {  
+  video.pause();
+}
+
+function openMenu(){
+  const menuNav = document.querySelectorAll('menuList');
+  menuNav.classList.toggle('open');
 }
 
 

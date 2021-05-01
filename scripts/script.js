5004ad568a05b900288b681767e2af413e08c975
Edwin Suesca <edwcarte16@gmail.com>
2021-05-01 03:11:59 -0500

    Animación botón menú hamburguesa

--- scripts/script.js
+++ scripts/script.js
@@ -20,9 +20,30 @@ function pauseFunction() {
   video.pause();
 }
 
+
+
 function openMenu(){
-  const menuNav = document.querySelectorAll('menuList');
-  menuNav.classList.toggle('open');
+
+  if (document.getElementById("menuListNav").style.clipPath != "circle(1000px at 100% 0%)") {
+    document.getElementById("menuListNav").style.clipPath = "circle(1000px at 100% 0%)";
+
+    document.getElementById("svgBurger").style = "height:22px; width:26px";
+
+    document.getElementById("rectBurgerMenu1").style = "rotate:-45deg; x:-12.999996px; y:11.246504px";
+
+    document.getElementById("rectBurgerMenu2").style = "rotate:45deg; x:1.2465px; y:-3px";
+
+  }
+
+  else {
+    document.getElementById("menuListNav").style.clipPath = "circle(0 at 100% 0%)";
+
+    document.getElementsByClassName("burgerMenuBox").style = "height:22px; width:26px";
+
+    document.getElementById("svgBurger").style = "height:22px; width:26px";
+    
+    document.getElementById("rectBurgerMenu1").style = "rotate:0; x:0; y:9.7727985px";
+
+    document.getElementById("rectBurgerMenu2").style = "rotate:0; x:0; y:0";
+  }
 }
-
-
